de6c89a2af3b47c4f4ddc5fa4c35a14d0ada1592
IljaDR <38908439+IljaDR@users.noreply.github.com>
2020-03-02 23:26:11 +0100

    Added github repo link to footer

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <footer class="w3-container w3-dark-grey w3-padding-32 w3-margin-top">
     <p style="text-align: center">Created with love, passion and copious amounts of stress and caffeine by Ilja De Rycke</p>
+    <p style="text-align: center">Source code can be found here: <a href="https://github.com/IljaDR/PHPBlog" target="_blank">github.com/IljaDR/PHPBlog</a></p>
     <p style="text-align: center">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
 </footer>
